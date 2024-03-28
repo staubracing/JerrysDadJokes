9ab25c754e8dbc2f0b90efdf612781af84237fb1
staubracing <staubracing@gmail.com>
2024-03-28 06:26:06 -0500

    Update button id and fix weather button functionality

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-document.getElementById('apiButton').addEventListener('click', function () {
+document.getElementById("jokeButton").addEventListener("click", function () {
   axios
-    .get('https://icanhazdadjoke.com/', {
+    .get("https://icanhazdadjoke.com/", {
       headers: {
-        Accept: 'application/json',
+        Accept: "application/json",
       },
     })
     .then((response) => {
       console.log(response.data.joke);
-      const jokeOutput = document.getElementById('jokeOutput');
+      const jokeOutput = document.getElementById("jokeOutput");
       jokeOutput.textContent = response.data.joke;
     })
-    .catch((error) => console.error('Error:', error));
+    .catch((error) => console.error("Error:", error));
 });
 
-// Add this to your script.js file
-document.getElementById('weatherButton').addEventListener('click', function () {
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("weatherButton").click();
   navigator.geolocation.getCurrentPosition(function (position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
 
-    axios.get(`https://api.weather.gov/points/${lat},${lon}`)
+    axios
+      .get(`https://api.weather.gov/points/${lat},${lon}`)
       .then(function (response) {
         var forecastUrl = response.data.properties.forecast;
         var city = response.data.properties.relativeLocation.properties.city;
@@ -29,7 +30,9 @@ document.getElementById('weatherButton').addEventListener('click', function () {
       })
       .then(function (data) {
         var forecast = data.forecast.data.properties.periods[0];
-        document.getElementById('weatherOutput').innerText = `In  ${data.city}, Today's forecast is ${forecast.shortForecast}, Temperature: ${forecast.temperature} ${forecast.temperatureUnit}`;
+        document.getElementById(
+          "weatherOutput"
+        ).innerText = `In  ${data.city}, Today's forecast is ${forecast.shortForecast}, Temperature: ${forecast.temperature} ${forecast.temperatureUnit}`;
       })
       .catch(function (error) {
         console.log(error);
