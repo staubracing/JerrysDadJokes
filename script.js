560f064124dd269e2425e3d5ab5fbf844b7ebff6
staubracing <staubracing@gmail.com>
2024-07-09 05:39:32 -0500

    add counter, favorites, CSS updates

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.getElementById("jokeButton").addEventListener("click", function () {
+let jokeCount = 0;
+let favorites = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const jokeButton = document.getElementById("jokeButton");
+  const jokeOutput = document.getElementById("jokeOutput");
+  const favoriteButton = document.getElementById("favoriteButton");
+  const favoritesList = document.getElementById("favoritesList");
+
+  jokeButton.addEventListener("click", fetchNewJoke);
+  favoriteButton.addEventListener("click", toggleFavorite);
+
+  fetchNewJoke();
+});
+
+function fetchNewJoke() {
   axios
     .get("https://icanhazdadjoke.com/", {
       headers: {
@@ -7,35 +22,45 @@ document.getElementById("jokeButton").addEventListener("click", function () {
     })
     .then((response) => {
       console.log(response.data.joke);
-      const jokeOutput = document.getElementById("jokeOutput");
       jokeOutput.textContent = response.data.joke;
+      jokeOutput.dataset.jokeId = response.data.id;
+      jokeCount++;
+      updateJokeCount();
+      updateFavoriteButton();
     })
     .catch((error) => console.error("Error:", error));
-});
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("weatherButton").click();
-  navigator.geolocation.getCurrentPosition(function (position) {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
+function updateJokeCount() {
+  document.getElementById("jokeCount").textContent = jokeCount;
+}
 
-    axios
-      .get(`https://api.weather.gov/points/${lat},${lon}`)
-      .then(function (response) {
-        var forecastUrl = response.data.properties.forecast;
-        var city = response.data.properties.relativeLocation.properties.city;
-        return axios.get(forecastUrl).then(function (forecastResponse) {
-          return { forecast: forecastResponse, city: city };
-        });
-      })
-      .then(function (data) {
-        var forecast = data.forecast.data.properties.periods[0];
-        document.getElementById(
-          "weatherOutput"
-        ).innerText = `In  ${data.city}, Today's forecast is ${forecast.shortForecast}, Temperature: ${forecast.temperature} ${forecast.temperatureUnit}`;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+function toggleFavorite() {
+  const jokeId = jokeOutput.dataset.jokeId;
+  const jokeText = jokeOutput.textContent;
+
+  const index = favorites.findIndex((fav) => fav.id === jokeId);
+  if (index > -1) {
+    favorites.splice(index, 1);
+  } else {
+    favorites.push({ id: jokeId, text: jokeText });
+  }
+
+  updateFavoriteButton();
+  updateFavoritesList();
+}
+
+function updateFavoriteButton() {
+  const jokeId = jokeOutput.dataset.jokeId;
+  const isFavorite = favorites.some((fav) => fav.id === jokeId);
+  favoriteButton.textContent = isFavorite ? "Remove from Favorites" : "Add to Favorites";
+}
+
+function updateFavoritesList() {
+  favoritesList.innerHTML = "";
+  favorites.forEach((fav) => {
+    const li = document.createElement("li");
+    li.textContent = fav.text;
+    favoritesList.appendChild(li);
   });
-});
+}
